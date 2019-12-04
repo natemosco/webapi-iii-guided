2740c4a7c7954daf7b1e895a8af27708eff0fb84
Nathaniel Mosco <natemosco@gmail.com>
2019-12-04 16:22:54 -0500

    updated notes and code from lecure

--- server.js
+++ server.js
@@ -7,35 +7,12 @@ const server = express();
 
 //middleware
 
-//custom middleware
-function logger(req, res, next) {
-  console.log(`${req.method} to ${req.originalUrl}`);
-  // res.send("all good");
-  next(); // allows the request to continue to the next middleware or route handler
-}
-
-function gatekeeper(req, res, next) {
-  if (req.headers.password === "mellon") {
-    next(); // allows the request to continue to the next middleware or route handler
-  } else {
-    res
-      .status(401)
-      .json({
-        errorMessage:
-          "Incorrect credentials, session may have timed-out. Please log back into account to continue to this page"
-      });
-  }
-}
-
-// write a gatekeeper middleware that reads a password from the headers and if the password is 'mellon', let it continue
-//if not, send back status code 401 and a message
-
 server.use(helmet()); // <<<<<<<<<<<<< use it 2:   GLOBAL
 server.use(express.json()); // built-in middleware
-server.use(logger());
+server.use(logger);
 
-// server.use('/api/hubs', helmet(), hubsRouter); //can use it for all routes
-server.use("/api/hubs", hubsRouter);
+// endpoints
+server.use("/api/hubs", checkRole("admin"), hubsRouter); // the router is local middleware, because it only applies to /api/hubs
 
 server.get("/", (req, res) => {
   const nameInsert = req.name ? ` ${req.name}` : "";
@@ -46,11 +23,48 @@ server.get("/", (req, res) => {
     `);
 });
 
+// commit
 server.get("/echo", (req, res) => {
   res.send(req.headers);
 });
 
-server.get("/area51", [helmet(), gatekeeper()], (req, res) => {
+// shift + alt + up (or down) to copy the selected lines
+server.get("/area51", gateKeeper, checkRole("agent"), (req, res) => {
   res.send(req.headers);
 });
+
 module.exports = server;
+
+//custom middleware
+function logger(req, res, next) {
+  console.log(`${req.method} to ${req.originalUrl}`);
+
+  // res.send("all good");   // this acts like a return and prevents any next middleware from functioning
+
+  next(); // allows the request to continue to the next middleware or route handler
+}
+
+// write a gatekeeper middleware that reads a password from the headers and if the password is 'mellon', let it continue
+//if not, send back status code 401 and a message
+
+function gatekeeper(req, res, next) {
+  if (req.headers.password === "mellon") {
+    next(); // allows the request to continue to the next middleware or route handler
+  } else {
+    res.status(401).json({
+      errorMessage:
+        "Incorrect credentials, session may have timed-out. Please log back into account to continue to this page"
+    });
+  }
+}
+
+// checkRole('admin'), checkRole('agents')
+function checkRole(role) {
+  return function(req, res, next) {
+    if (role && role === req.headers.role) {
+      next();
+    } else {
+      res.status(403).json({ message: "can't touch this!" });
+    }
+  };
+}
